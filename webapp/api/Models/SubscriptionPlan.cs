bf062681014d2864334b530705bbb247485bcf6a
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-22 16:40:00 +0300

    Create SubscriptionPlan class

---
+++ webapp/api/Models/SubscriptionPlan.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapp.api.Models;
+
+public class SubscriptionPlan
+{
+    public int Id { get; set; }
+    public int ServiceId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18,2")] public decimal Price { get; set; }
+    public string Duration { get; set; } = string.Empty;
+    public string Features { get; set; } = string.Empty;
+
+    public required Service Service;
+}
