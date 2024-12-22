1df86df275f9ff5a011c3e53fc9b0513fd6e0bba
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-22 16:39:40 +0300

    Create Service class

---
+++ webapp/api/Models/Service.cs
@@ -0,0 +1,9 @@
+﻿namespace webapp.api.Models;
+
+public class Service
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description = string.Empty;
+    public List<SubscriptionPlan> SubscriptionPlans { get; set; } = [];
+}
