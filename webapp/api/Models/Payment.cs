33c8ece1d0b68905195f89781fac89362f44338b
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-22 16:39:29 +0300

    Create Payment class

---
+++ webapp/api/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapp.api.Models;
+
+public class Payment
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int SubscriptionId { get; set; }
+    [Column(TypeName = "decimal(18,2)")] public decimal Amount { get; set; }
+    public DateTime PaymentDate;
+    public string PaymentMethod { get; set; } = string.Empty;
+}
