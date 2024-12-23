75c896b1b008151de10436b6e47119d61a13fb37
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-23 16:28:24 +0300

    Rename Service model to Platform and add max length constraints

---
+++ webapp/api/Model/Platform.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapp.api.Model;
+
+public class Platform
+{
+    public int Id { get; set; }
+    [MaxLength(100)] public string Name { get; set; } = string.Empty;
+    public string Description = string.Empty;
+    public List<SubscriptionPlan> SubscriptionPlans { get; set; } = [];
+}
--- webapp/api/Model/Service.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webapp.api.Model;
-
-public class Service
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description = string.Empty;
-    public List<SubscriptionPlan> SubscriptionPlans { get; set; } = [];
-}
