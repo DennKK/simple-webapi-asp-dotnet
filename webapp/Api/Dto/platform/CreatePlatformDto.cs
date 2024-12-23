8e171803606bc8ac99df39592b97988ff59e856a
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-23 19:50:11 +0300

    Add CreatePlatform DTO

---
+++ webapp/Api/Dto/platform/CreatePlatformDto.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapp.Api.Dto.platform;
+
+public record CreatePlatformDto(
+    [MaxLength(32)] string Name,
+    [MaxLength(512)] string Description
+);
