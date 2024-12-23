8ec096b594a4e53c9101062d9f42ff8d9f0a0d05
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-23 19:44:07 +0300

    Add links to entities and fix constraints

--- webapp/Api/Model/Payment.cs
+++ webapp/Api/Model/Payment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace webapp.Api.Model;
 
@@ -8,6 +9,9 @@ public class Payment
     public int UserId { get; set; }
     public int SubscriptionId { get; set; }
     [Column(TypeName = "decimal(18,2)")] public decimal Amount { get; set; }
-    public DateTime PaymentDate;
-    public string PaymentMethod { get; set; } = string.Empty;
+    public DateTime PaymentDate { get; set; }
+    [MaxLength(64)] public string PaymentMethod { get; set; } = string.Empty;
+
+    public required User User { get; set; }
+    public required Subscription Subscription { get; set; }
 }
