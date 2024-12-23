7a8c6f1b014ce311619b764327ca4295d967eb4d
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-23 16:28:39 +0300

    Add platform controller

---
+++ webapp/api/Controller/PlatformController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapp.api.Service.Platform;
+
+namespace webapp.api.Controller;
+
+[ApiController]
+[Route("api/v1/platforms")]
+public class PlatformController(IPlatformService platformService) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAllPlatforms()
+    {
+        var platforms = platformService.GetAllPlatforms();
+        return Ok(platforms);
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetPlatform([FromRoute] int id)
+    {
+        var platform = platformService.GetPlatform(id);
+        return Ok(platform);
+    }
+}
