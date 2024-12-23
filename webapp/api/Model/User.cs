5f93785939a4bb64e41f6658a3425fc424812189
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-23 16:30:06 +0300

    Add max length constraints for string fields

--- webapp/api/Model/User.cs
+++ webapp/api/Model/User.cs
@@ -1,10 +1,12 @@
-﻿namespace webapp.api.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapp.api.Model;
 
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
+    [MaxLength(100)] public string Username { get; set; } = string.Empty;
+    [MaxLength(100)] public string Email { get; set; } = string.Empty;
     public DateTime CreatedAt { get; } = DateTime.UtcNow;
     public List<Subscription> Subscriptions { get; set; } = [];
 }
