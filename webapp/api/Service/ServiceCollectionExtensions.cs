5c4b9d6d3e32d8e46fcdb6a9dc20fd2c492c91b7
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-23 16:33:04 +0300

    Create service collection extension for more convenient registration of services

---
+++ webapp/api/Service/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using webapp.api.Service.Platform;
+
+namespace webapp.api.Service;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IPlatformService, PlatformService>();
+    }
+}
