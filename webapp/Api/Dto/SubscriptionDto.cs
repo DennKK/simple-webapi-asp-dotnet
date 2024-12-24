c2533ee4c8a7fcaa8df0cb1932903968cea110a9
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-24 10:38:17 +0300

    Create subscription dto

---
+++ webapp/Api/Dto/SubscriptionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace webapp.Api.Dto;
+
+public record SubscriptionDto(
+    int Id,
+    string Name,
+    decimal Price,
+    DateTime StartDate,
+    DateTime EndDate,
+    string Status
+);
