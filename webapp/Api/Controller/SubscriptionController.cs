edea1a0c36d256c99b83d49b39152825235a70e0
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-24 12:45:20 +0300

    Create subscription controller

---
+++ webapp/Api/Controller/SubscriptionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapp.Api.Dto.Subscription;
+using webapp.Api.Service.Subscription;
+
+namespace webapp.Api.Controller;
+
+[ApiController]
+[Route("api/v1/subscriptions")]
+public class SubscriptionController(ISubscriptionService subscriptionService) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAllPlatforms()
+    {
+        return Ok(subscriptionService.GetAllSubscriptions());
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetSubscription([FromRoute] int id)
+    {
+        return Ok(subscriptionService.GetSubscription(id));
+    }
+
+    [HttpPost]
+    public IActionResult CreateSubscription([FromBody] CreateSubscriptionDto createSubscriptionDto)
+    {
+        return Ok(subscriptionService.CreateSubscription(createSubscriptionDto));
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateSubscription([FromRoute] int id, [FromBody] UpdateSubscriptionDto updateSubscriptionDto)
+    {
+        return Ok(subscriptionService.UpdateSubscription(id, updateSubscriptionDto));
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteSubscription([FromRoute] int id)
+    {
+        subscriptionService.DeleteSubscription(id);
+        return NoContent();
+    }
+}
