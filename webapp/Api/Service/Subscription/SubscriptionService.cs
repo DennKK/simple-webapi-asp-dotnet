6f8f5316cb94a6fc62901accac7f15537a0a34ae
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-24 12:46:12 +0300

    Create subscription service

---
+++ webapp/Api/Service/Subscription/SubscriptionService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using webapp.Api.Dto.Subscription;
+using webapp.Api.Repository.Subscription;
+using webapp.Api.Service.Platform;
+
+namespace webapp.Api.Service.Subscription;
+
+public class SubscriptionService(
+    ISubscriptionRepository repository,
+    IPlatformService platformService,
+    IMapper mapper) : ISubscriptionService
+{
+    public async Task<List<SubscriptionDto>> GetAllSubscriptions()
+    {
+        var subscriptions = await repository.GetAllSubscriptionsAsync();
+        return mapper.Map<List<SubscriptionDto>>(subscriptions);
+    }
+
+    public async Task<SubscriptionDto> GetSubscription(int id)
+    {
+        var subscription = await FindSubscriptionById(id);
+        return mapper.Map<SubscriptionDto>(subscription);
+    }
+
+    public async Task<SubscriptionDto> CreateSubscription(CreateSubscriptionDto createSubscriptionDto)
+    {
+        await platformService.GetPlatform(createSubscriptionDto.PlatformId);
+
+        var subscription = mapper.Map<Model.Subscription>(createSubscriptionDto);
+        await repository.AddSubscriptionAsync(subscription);
+        return mapper.Map<SubscriptionDto>(subscription);
+    }
+
+    public async Task<SubscriptionDto> UpdateSubscription(int id, UpdateSubscriptionDto updateSubscriptionDto)
+    {
+        var subscription = await FindSubscriptionById(id);
+        mapper.Map(updateSubscriptionDto, subscription);
+        await repository.UpdateSubscriptionAsync(subscription);
+        return mapper.Map<SubscriptionDto>(subscription);
+    }
+
+    public async Task DeleteSubscription(int id)
+    {
+        var platform = await FindSubscriptionById(id);
+        await repository.DeleteSubscriptionAsync(platform);
+    }
+
+    private async Task<Model.Subscription> FindSubscriptionById(int id)
+    {
+        return await repository.GetSubscriptionByIdAsync(id) ??
+               throw new InvalidOperationException($"Subscription with id {id} does not exist.");
+    }
+}
