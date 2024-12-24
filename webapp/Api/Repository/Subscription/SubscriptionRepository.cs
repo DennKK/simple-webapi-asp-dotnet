aacd6d8e379e0c96f2ea4c8b03584be3626e3b1c
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-24 12:47:04 +0300

    Create subscription repository

---
+++ webapp/Api/Repository/Subscription/SubscriptionRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapp.Api.Data;
+
+namespace webapp.Api.Repository.Subscription;
+
+public class SubscriptionRepository(ApplicationDbContext dbContext) : ISubscriptionRepository
+{
+    public async Task<List<Model.Subscription>> GetAllSubscriptionsAsync()
+    {
+        return await dbContext.Subscriptions.ToListAsync();
+    }
+
+    public async Task<Model.Subscription?> GetSubscriptionByIdAsync(int id)
+    {
+        return await dbContext.Subscriptions.FindAsync(id);
+    }
+
+    public async Task AddSubscriptionAsync(Model.Subscription subscription)
+    {
+        dbContext.Subscriptions.Add(subscription);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateSubscriptionAsync(Model.Subscription subscription)
+    {
+        dbContext.Subscriptions.Update(subscription);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteSubscriptionAsync(Model.Subscription subscription)
+    {
+        dbContext.Subscriptions.Remove(subscription);
+        await dbContext.SaveChangesAsync();
+    }
+}
