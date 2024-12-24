26884b9996a1ef01fb6d5194d5f2fbddfa123560
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-24 12:50:17 +0300

    Create subscription repository interface

---
+++ webapp/Api/Repository/Subscription/ISubscriptionRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace webapp.Api.Repository.Subscription;
+
+public interface ISubscriptionRepository
+{
+    Task<List<Model.Subscription>> GetAllSubscriptionsAsync();
+    Task<Model.Subscription?> GetSubscriptionByIdAsync(int id);
+    Task AddSubscriptionAsync(Model.Subscription subscription);
+    Task UpdateSubscriptionAsync(Model.Subscription subscription);
+    Task DeleteSubscriptionAsync(Model.Subscription subscription);
+}
