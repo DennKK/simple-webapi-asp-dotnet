24ffdc3c52fc8b3e91c0d6ebbbe1879d6cf33365
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-24 12:50:30 +0300

    Add create sub dto

---
+++ webapp/Api/Dto/Subscription/CreateSubscriptionDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapp.Api.Dto.Subscription;
+
+public record CreateSubscriptionDto(
+    [Required] int PlatformId,
+    [Required] [MaxLength(32)] string Name,
+    decimal Price,
+    [Required] DateTime StartDate,
+    DateTime? EndDate,
+    bool IsActive = true
+);
