2bc1fadb8e11be060d4f9b16e97024290506c85c
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-24 10:39:23 +0300

    Rename to SubscriptionMappingProfile

---
+++ webapp/Api/Service/Mapper/SubscriptionMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using webapp.Api.Dto;
+using webapp.Api.Model;
+
+namespace webapp.api.Service.Mapper;
+
+public class SubscriptionMappingProfile : Profile
+{
+    public SubscriptionMappingProfile()
+    {
+        CreateMap<SubscriptionDto, Subscription>();
+        CreateMap<Subscription, SubscriptionDto>();
+    }
+}
