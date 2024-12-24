ae0a47a8f083433b1d4deb98f056fe7fb040c88c
Denis Karpov <karpovdenis.al@gmail.com>
2024-12-24 12:50:08 +0300

    Create subscription service interface

---
+++ webapp/Api/Service/Subscription/ISubscriptionService.cs
@@ -0,0 +1,12 @@
+﻿using webapp.Api.Dto.Subscription;
+
+namespace webapp.Api.Service.Subscription;
+
+public interface ISubscriptionService
+{
+    Task<List<SubscriptionDto>> GetAllSubscriptions();
+    Task<SubscriptionDto> GetSubscription(int id);
+    Task<SubscriptionDto> CreateSubscription(CreateSubscriptionDto createSubscriptionDto);
+    Task<SubscriptionDto> UpdateSubscription(int id, UpdateSubscriptionDto updateSubscriptionDto);
+    Task DeleteSubscription(int id);
+}
